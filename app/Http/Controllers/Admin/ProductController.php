e61b46d0a6927e08eb5afbe48730fc78bdc35667
Jose Antonio Rojas <joseucro1@gmail.com>
2023-11-20 22:03:52 -0400

    products add

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -667,7 +667,7 @@ class ProductController extends Controller
                             $id = DB::table('products')->insertGetId($data);
                             
                             $productSave = Product::find($id);
-                            dd($productSave);
+                            dd($category);
                         
                             $productSave->categories()->attach([$category[0]->id]);
                             $productSave->tags()->attach([1]);
